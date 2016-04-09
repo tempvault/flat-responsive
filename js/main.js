cbca098f9171a09397308d68e159e3f39ffccc0e
bmcculley <bmcculley@outlook.com>
2016-04-09 01:49:27 -0400

    mobile menu, colors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$( document ).ready(function() {
+	$('.navbar-btn').click(function() {
+		if ( $('.main-menu').is( ':hidden' ) ) {
+			$( '.main-menu' ).slideDown( 'slow' );
+		} else {
+			$( '.main-menu' ).slideUp('slow');
+		}
+	});
+	var windowWidth = $( window ).width();
+	if (windowWidth < 480) {
+		$('.main-menu').hide();
+	}
+	
+	$( window ).resize(function() {
+		var windowWidth = $( window ).width();
+		//$('#ww').text( "The window width is " + windowWidth + "px." );
+		if (windowWidth >= 480) {
+			$('.main-menu').show();
+		}
+		else {
+			$('.main-menu').hide();
+		}
 
+	});
+});
